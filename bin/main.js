4e38603e401ce8886833cbe679ac0b4e88bd1835
Guilherme Rialli Oliveira <gui.rialli@gmail.com>
2023-10-02 15:21:19 -0300

    trocando exec por spawn e manipulando o erro

--- bin/main.js
+++ bin/main.js
@@ -5,21 +5,26 @@ const inputFormat = args[0] === undefined ? "png" : args[0];
 const outputFormat = args[1] === undefined ? "webp" : args[1];
 
 const convertFfmpeg = (file_name) => {
-    child_process
-        .exec(`ffmpeg -i ${file_name}.${inputFormat} ${file_name}.${outputFormat}`, (err) => {
-            if (err) throw err;
-            console.log(`${file_name} foi convertido com sucesso para ${outputFormat}`);
-        })
+    const ffmpeg = child_process.spawn("ffmpeg", [
+        `-i`,
+        `${file_name}.${inputFormat}`,
+        `${file_name}.${outputFormat}`,
+    ]);
 
+    ffmpeg.on("close", () => {
+        console.log(`${file_name} convertido com sucesso para ${outputFormat}`);
+    });
+
+    ffmpeg.on("error", (err) => { console.log(`${file_name} error ao converter para ${outputFormat}\n${err.message}\n`); });
 };
 
 fs.readdir(".", (err, data) => {
     if (err) throw err;
     data.forEach((file) => {
         const arr_file = file.split(".");
-        if (arr_file.length > 1 && arr_file[arr_file.length - 1] === inputFormat){
-            if(fs.existsSync(`${arr_file[0]}.${outputFormat}`)){
-                fs.unlinkSync(`${arr_file[0]}.${outputFormat}`)
+        if (arr_file.length > 1 && (arr_file[arr_file.length - 1] === inputFormat || '.' + arr_file[arr_file.length - 1] === inputFormat)) {
+            if (fs.existsSync(`${arr_file[0]}.${outputFormat}`)) {
+                fs.unlinkSync(`${arr_file[0]}.${outputFormat}`);
             }
             convertFfmpeg(arr_file[0]);
         }
